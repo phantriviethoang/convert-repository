eb4a8274697077983056fe854fe8904b8d043f2d
Hoang <humhliao@gmail.com>
2024-11-30 14:11:52 +0700

    Edit done

--- convert.ps1
+++ convert.ps1
@@ -1,7 +1,1 @@
-# Liệt kê tất cả các repo công khai và chuyển thành riêng tư
-$repos = gh repo list buddybaddog --visibility public --limit 100 | ForEach-Object { $_.Split(" ")[0] }
-
-foreach ($repo in $repos) {
-    gh repo edit $repo --visibility private
-    Write-Host "Đã chuyển repository $repo thành riêng tư."
-}
+$repos = gh repo list buddybaddog --visibility public --json name -q ".[].name"
